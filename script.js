47db4d1e41ff8daf0459c28c74a3bf78f78de174
V <vriver682@gmail.com>
2023-05-30 11:52:59 -0700

    updated script.js

---
+++ script.js
@@ -0,0 +1,23 @@
+const data = {
+    brandName: "Smash Games",
+    pages: [
+        {
+            name: "index",
+            pageName: "Home",
+            callToAction: {
+                src: "images/SmashGamesLogo-short.jpg",
+                altText: "Smash Games Logo"
+            }
+        },
+        {
+            name: "about",
+            pageName: "About",
+            callToAction: {
+                src: "images/Inferno-Jumbotron.jpg",
+                altText: "Inferno Blast Gameplay",
+                buttonLinkSrc: "https://steampowered.com",
+                buttonLinkText: "Buy now on Steam!"
+            }
+        },
+    ]
+}
