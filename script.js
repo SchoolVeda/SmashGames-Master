04fc432fc2f70c82bea1d1fc8802989d30e5f06c
V <vriver682@gmail.com>
2023-05-30 11:04:28 -0700

    added script.js

---
+++ script.js
@@ -0,0 +1,25 @@
+const data = {
+    cards: [
+        {
+            imgPath: "./somewhere.img",
+            imgAltText: "some alternative text",
+            title: "title text",
+            body: "body text here",
+            actionLink: "buttonlink"
+        },
+        {
+            imgPath: "./somewhere.img",
+            imgAltText: "some alternative text",
+            title: "title text",
+            body: "body text here",
+            actionLink: "buttonlink"
+        },
+        {
+            imgPath: "./somewhere.img",
+            imgAltText: "some alternative text",
+            title: "title text",
+            body: "body text here",
+            actionLink: "buttonlink"
+        },
+    ]
+}s
